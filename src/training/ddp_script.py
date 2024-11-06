a3df698fc457a63790be26f5777dac5d16ca437e
That1Panda <pandahd75@gmail.com>
2024-11-06 16:25:37 +0200

    [Fix] Handling torchrun command inputs

--- src/training/ddp_script.py
+++ src/training/ddp_script.py
@@ -1,23 +1,26 @@
+import os
 import torch
-import torch.multiprocessing as mp
-
 from src.training.ddp_train import DDPTrain
 
 
 class DDP:
     def __init__(self):
-        self.world_size = torch.cuda.device_count()
-        if self.world_size < 2:
-            raise ValueError("This script is intended to be run with at least 2 GPUs")
-
-    def main_worker(self, rank):
-        ddp_trainer = DDPTrain(rank)
-        ddp_trainer.model_training()
+        self.rank = int(
+            os.getenv("LOCAL_RANK", 0)
+        )  # Local rank for the current process
+        self.world_size = int(os.getenv("WORLD_SIZE", 1))  # Total number of processes
+        if self.world_size <= 1:
+            raise ValueError(
+                f"Number of processes should be greater than 1, got {self.world_size}"
+            )
 
     def run(self):
-        print(f"Starting DDP training with {self.world_size} GPUs")
-        mp.spawn(self.main_worker, nprocs=self.world_size, join=True)
-        print("DDP training completed")
+        print(
+            f"Starting DDP training on rank {self.rank} out of {self.world_size} processes"
+        )
+        ddp_trainer = DDPTrain(self.rank)
+        ddp_trainer.model_training()
+        print(f"DDP training completed on rank {self.rank}")
 
 
 if __name__ == "__main__":
