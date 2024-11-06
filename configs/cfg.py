09ab1a26090d80216db60afc495d93f3a619b1f2
That1Panda <pandahd75@gmail.com>
2024-11-06 12:40:28 +0200

    [Development] Adding DataParallel class and its configs

--- configs/cfg.py
+++ configs/cfg.py
@@ -7,7 +7,9 @@ PROJECT_DIR = Path(__file__).parent.parent
 TEST_DATA_DIR = PROJECT_DIR / "tests" / "data"
 DATA_PATH = PROJECT_DIR / "data"
 CONFIGS_DIR = PROJECT_DIR / "configs"
+LOG_DIR = PROJECT_DIR / "logs"
+
 VGG16_CONFIG_PATH = CONFIGS_DIR / "vgg16_config.yaml"
 DATA_CONFIG_PATH = CONFIGS_DIR / "data_config.yaml"
 SINGLE_GPU_TRAIN_CONFIG_PATH = CONFIGS_DIR / "single_gpu_train_config.yaml"
-LOG_DIR = PROJECT_DIR / "logs"
+DATA_PARALLEL_CONFIG_PATH = CONFIGS_DIR / "data_parallel_train_config.yaml"
